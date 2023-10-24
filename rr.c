1c1a3dd5847bf7e1950b2aab0d7306715b61ae25
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2023-10-24 11:46:24 -0700

    finished static quantum size

--- rr.c
+++ rr.c
@@ -23,11 +23,36 @@ struct process
   long start_exec_time;
   long waiting_time;
   long response_time;
+  long completion_time;
+  long cpu_consumption;
+  int array_placeholder;
+  bool first_queue;
+  bool first_run;
   /* End of "Additional fields here" */
 };
 
 TAILQ_HEAD (process_list, process);
+int compare(const void *a, const void *b) {
+    long longA = *((long *)a);
+    long longB = *((long *)b);
+    return (longA > longB) - (longA < longB);
+}
 
+long calculateMedian(long *array, int size) {
+    // Sort the array in ascending order
+    qsort(array, size, sizeof(long), compare);
+
+    // Calculate the median
+    if (size % 2 == 1) {
+        // If the number of elements is odd, return the middle element
+        return (long)array[size / 2];
+    } else {
+        // If the number of elements is even, return the average of the two middle elements
+        long middle1 = array[size / 2 - 1];
+        long middle2 = array[size / 2];
+        return (long)(middle1 + middle2) / 2;
+    }
+}
 /* Skip past initial nondigits in *DATA, then scan an unsigned decimal
    integer and return its value.  Do not scan past DATA_END.  Return
    the integer’s value.  Report an error and exit if no integer is
@@ -183,11 +208,77 @@ main (int argc, char *argv[])
   long total_response_time = 0;
 
   /* Your code here */
-  for (long i = 0; i < ps.nprocesses; i++) {
-      printf("%d", ps.process[i].pid);
-      printf("%d", ps.process[i].arrival_time);
-      printf("%d", ps.process[i].burst_time);
+  bool dynamic = false;
+  if (quantum_length <= 0) {
+      quantum_length = 1;
+      dynamic = true;
   }
+  long* cpu_arr = (long *)malloc(ps.nprocesses * sizeof(long));
+  for (long i = 0; i < ps.nprocesses; i++) {
+      ps.process[i].first_queue = true;
+      ps.process[i].remaining_time = ps.process[i].burst_time;
+      ps.process[i].cpu_consumption = 0;
+      ps.process[i].first_run = false;
+      ps.process[i].array_placeholder = i;
+  }
+    long timer = ps.process[0].arrival_time;
+    long numOfProcessesArrived = 0;
+    TAILQ_INSERT_TAIL(&list, &ps.process[0], pointers);
+    while (!TAILQ_EMPTY(&list)) {
+        struct process *current_process = TAILQ_FIRST(&list);
+        TAILQ_REMOVE(&list, current_process, pointers); // pop off queue
+        if(!current_process->first_run) {
+            total_response_time += timer - current_process->arrival_time;
+            current_process->first_run = true;
+        }
+        if (current_process->remaining_time <= quantum_length && current_process->arrival_time <= timer) {
+            long current_time = timer;
+            while(timer <= current_time + current_process->remaining_time) {
+                for (long i = 1; i < ps.nprocesses; i++) {
+                    if(ps.process[i].first_queue && ps.process[i].arrival_time <= timer) {
+                        TAILQ_INSERT_TAIL(&list, &ps.process[i], pointers);
+                        numOfProcessesArrived++;
+                        ps.process[i].first_queue = false;
+                    }
+                }
+                timer++;
+            }
+            timer--;
+            current_process->completion_time = timer;
+            total_wait_time += current_process->completion_time - current_process->arrival_time - current_process->burst_time;
+            current_process->cpu_consumption += current_process->remaining_time;
+            numOfProcessesArrived--;
+        }
+        else if (current_process->arrival_time <= timer) {
+            long current_time = timer;
+            while(timer <= current_time + quantum_length) {
+                for (long i = 1; i < ps.nprocesses; i++) {
+                    if(ps.process[i].first_queue && ps.process[i].arrival_time <= timer) {
+                        TAILQ_INSERT_TAIL(&list, &ps.process[i], pointers);
+                        numOfProcessesArrived++;
+                        ps.process[i].first_queue = false;
+                    }
+                }
+                timer++;
+            }
+            timer--;
+            current_process->remaining_time = current_process->remaining_time - quantum_length;
+            current_process->cpu_consumption += quantum_length;
+            TAILQ_INSERT_TAIL(&list, current_process, pointers);
+        }
+        cpu_arr[current_process->array_placeholder] = current_process->cpu_consumption;
+        if (dynamic) {
+            long median = calculateMedian(cpu_arr, numOfProcessesArrived);
+            quantum_length = median;
+            if (quantum_length == 0) {
+                quantum_length = 1;
+            }
+        }
+        if(numOfProcessesArrived > 1) {
+            timer++; // takes into account context switch time if there is more than one process in queue
+        }
+    }
+    free(cpu_arr);
   /* End of "Your code here" */
 
   printf ("Average wait time: %.2f\n",
@@ -204,3 +295,8 @@ main (int argc, char *argv[])
   free (ps.process);
   return 0;
 }
+
+
+
+
+
